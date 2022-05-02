bc52ce861241a7b96e617cf7c18f4475a6e96055
Sveta077 <pugach.svetka@gmail.com>
2022-05-02 18:10:19 +0200

    fix: added functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,22 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
-  
+  //throw new NotImplementedError('Not implemented');
+  let age;
+  if ( typeof(sampleActivity) === 'string' && +sampleActivity > 0 ) {
+    let value = 0.693 / HALF_LIFE_PERIOD;
+    age = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / value);
+    if ( age < 0) {
+      return false
+    }
+    return age
+  } else {
+    return false
+  }
+}
+
+module.exports = {
+  dateSample
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
+  let str = n.toString();
+  let arr = [];
+  for(let i = 0; i < str.length; i++) {
+    arr.push(str.replace(str[i], ''));
+  }
+  let max = +arr[0];
+  for (let j = 1; j < arr.length; j++) {
+    if (max < +arr[j]) {
+      max = +arr[j];
+    }
+  }
+
+  return max;
+}
+
+module.exports = {
+  deleteDigit
 }
 
 module.exports = {
