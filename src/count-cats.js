ecdd860b3e72724a0f8d7bfcfbb9970e450f20f9
Sveta077 <pugach.svetka@gmail.com>
2022-05-02 18:01:37 +0200

    fix: added functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,7 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   let cats = 0;
   for(let i = 0; i < matrix.length; i++) {
     for(let j = 0; j < matrix[i].length; j++) {
